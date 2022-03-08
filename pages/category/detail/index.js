7868a925fc15270b3f93df30087f2572cebf51fd
fahmiluthfillah <fahmi.luthfillah@gmail.com>
2022-03-08 09:26:32 +0700

    update page error v2

--- pages/category/detail/index.js
+++ pages/category/detail/index.js
@@ -1,3 +1,15 @@
-export default function detailCategory() {
-    
+import Image from "next/image";
+import Container from "../../components/container";
+import HeadTitle from "../../components/headTitle";
+import TitleBar from "../../components/titleBar";
+
+export default function DetailCategory() {
+  return (
+    <div className="h-auto">
+      <TitleBar titleBar="Menu" />
+      <div className="pt-5">
+        <Container>List Menu by Category</Container>
+      </div>
+    </div>
+  );
 }
