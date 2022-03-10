255ca08f080719db4cfba6365465736d7b405266
fahmiluthfillah <fahmi.luthfillah@gmail.com>
2022-03-10 23:31:14 +0700

    Update view category

--- pages/category/index.js
+++ pages/category/index.js
@@ -3,11 +3,6 @@ import Container from "../../components/container";
 import HeadTitle from "../../components/headTitle";
 import TitleBar from "../../components/titleBar";
 
-import Waterfall from "../../public/img/waterfall.svg";
-import Mountain from "../../public/img/mountain.svg";
-import Pools from "../../public/img/swimming-pool.svg";
-import Guide from "../../public/img/tour-guide.svg";
-
 export default function Category() {
   return (
     <div className="h-auto">
@@ -48,56 +43,68 @@ export default function Category() {
               </p>
             </div>
           </div>
-          <div className="box bg-grey rounded-lg m-3 h-52 flex items-center justify-center text-black">
-            <div>
-              <div className="w-32 h-32 relative m-auto mt-2">
-                <Image
-                  src="https://i1.wp.com/bondowosotourism.com/wp-content/uploads/2019/10/3.jpg?w=900&ssl=1"
-                  layout="fill"
-                  className="object-cover rounded w-full h-full"
-                  alt="fav-1"
-                />
-              </div>
-              <p className="text-xs text-center text-gray-500 mt-2">Mountain</p>
-              <p className="text-sm text-center text-gray-500 mt-2 mb-1">
+
+          <div className="h-48 w-44 m-auto rounded overflow-hidden shadow-lg">
+            <img
+              className="w-full"
+              src="https://i1.wp.com/bondowosotourism.com/wp-content/uploads/2019/10/3.jpg?w=900&ssl=1"
+              alt="Mountain"
+            />
+            <div className="px-6 py-4">
+              <div className="text-xs text-center mb-2">Mountain</div>
+              <p className="text-gray-700 text-base text-center font-bold">
                 Kawah Wurung
               </p>
             </div>
           </div>
-          <div className="box bg-grey rounded-lg m-3 h-52 flex items-center justify-center text-black">
-            <div>
-              <div className="w-32 h-32 relative m-auto mt-2">
-                <Image
-                  src="https://i1.wp.com/bondowosotourism.com/wp-content/uploads/2019/10/3.jpg?w=900&ssl=1"
-                  layout="fill"
-                  className="object-cover rounded w-full h-full"
-                  alt="fav-1"
-                />
-              </div>
-              <p className="text-xs text-center text-gray-500 mt-2">Mountain</p>
-              <p className="text-sm text-center text-gray-500 mt-2 mb-1">
+          <div className="h-48 w-44 m-auto rounded overflow-hidden shadow-lg">
+            <img
+              className="w-full"
+              src="https://i1.wp.com/bondowosotourism.com/wp-content/uploads/2019/10/3.jpg?w=900&ssl=1"
+              alt="Mountain"
+            />
+            <div className="px-6 py-4">
+              <div className="text-xs text-center mb-2">Mountain</div>
+              <p className="text-gray-700 text-base text-center font-bold">
                 Kawah Wurung
               </p>
             </div>
           </div>
-          <div className="box bg-grey rounded-lg m-3 h-52 flex items-center justify-center text-black">
-            <div>
-              <div className="w-32 h-32 relative m-auto mt-2">
-                <Image
-                  src="https://i1.wp.com/bondowosotourism.com/wp-content/uploads/2019/10/3.jpg?w=900&ssl=1"
-                  layout="fill"
-                  className="object-cover rounded w-full h-full"
-                  alt="fav-1"
-                />
-              </div>
-              <p className="text-xs text-center text-gray-500 mt-2">Mountain</p>
-              <p className="text-sm text-center text-gray-500 mt-2 mb-1">
+          <div className="h-56 w-44 m-auto rounded overflow-hidden shadow-lg">
+            <img
+              className="w-full"
+              src="https://i1.wp.com/bondowosotourism.com/wp-content/uploads/2019/10/3.jpg?w=900&ssl=1"
+              alt="Mountain"
+            />
+            <div className="px-6 py-4">
+              <div className="text-xs text-center mb-2">Mountain</div>
+              <p className="text-gray-700 text-base text-center font-bold pb-2">
                 Kawah Wurung
               </p>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #photography
+              </span>
+            </div>
+          </div>
+          <div className="h-56 w-44 m-auto rounded overflow-hidden shadow-lg">
+            <img
+              className="w-full"
+              src="https://i1.wp.com/bondowosotourism.com/wp-content/uploads/2019/10/3.jpg?w=900&ssl=1"
+              alt="Mountain"
+            />
+            <div className="px-6 py-4">
+              <div className="text-xs text-center mb-2">Mountain</div>
+              <p className="text-gray-700 text-base text-center font-bold pb-2">
+                Kawah Wurung
+              </p>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #photography
+              </span>
             </div>
           </div>
         </div>
       </div>
+      
     </div>
   );
 }
